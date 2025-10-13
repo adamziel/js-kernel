17ea15a38a3ad3c9a61472364ce745c06f367a7d
Adam Zieliński <adam@adamziel.com>
2025-10-13 19:10:23 +0200

    Example with mockShell call

--- main-app.ts
+++ main-app.ts
@@ -11,34 +11,60 @@ for (const [name, source] of Object.entries(busyboxPrograms)) {
 	kernel.writeFileSync(`/bin/${name}`, `${source}\n`, { mode: 0o755 })
 }
 
-const lsWorker = kernel.spawn({
-	argv: ['ls', '/bin'],
-	env: {},
-	cwd: '/',
-	name: 'ls-demo',
-	stdio: {
-		stdout: 'pipe',
-		stderr: 'pipe',
-	},
-	debug: false,
-})
+function mockShell(argv: string[]) {
+	const worker = kernel.spawn({
+		argv,
+		env: {},
+		cwd: '/',
+		name: 'shell',
+	});
 
-if (typeof lsWorker === 'number') {
-	console.error(
-		'Failed to spawn ls process',
-		lsWorker === ExitCode.NOT_FOUND ? 'not found' : 'error'
-	)
-} else {
-	const decodeChunk = (chunk: KernelStdioChunk) =>
-		typeof chunk === 'string' ? chunk : new TextDecoder().decode(chunk)
-
-	lsWorker.stdout?.on('data', (chunk) => {
-		console.log('[ls stdout]', decodeChunk(chunk))
-	})
-	lsWorker.stderr?.on('data', (chunk) => {
-		console.error('[ls stderr]', decodeChunk(chunk))
-	})
-	lsWorker.onExit((code) => {
-		console.log('ls exited with code', code)
+	return new Promise((resolve) => {
+		worker.onExit((code) => {
+			// console.log('ls exited with code', code)
+			resolve(code)
+		})
 	})
 }
+
+await mockShell(['ls', '/']);
+await mockShell(['mkdir', '/test']);
+await mockShell(['touch', '/test/file.txt']);
+await mockShell(['ls', '/test']);
+await mockShell(['mv', '/test', '/test2']);
+await mockShell(['ls', '/test2']);
+await mockShell(['rm', '-R', '/test2']);
+// await mockShell(['ls', '/']);
+
+
+// const lsWorker = kernel.spawn({
+// 	argv: ['ls', '/'],
+// 	env: {},
+// 	cwd: '/',
+// 	name: 'ls-demo',
+// 	stdio: {
+// 		stdout: 'pipe',
+// 		stderr: 'pipe',
+// 	},
+// 	debug: false,
+// })
+
+// if (typeof lsWorker === 'number') {
+// 	console.error(
+// 		'Failed to spawn ls process',
+// 		lsWorker === ExitCode.NOT_FOUND ? 'not found' : 'error'
+// 	)
+// } else {
+// 	const decodeChunk = (chunk: KernelStdioChunk) =>
+// 		typeof chunk === 'string' ? chunk : new TextDecoder().decode(chunk)
+
+// 	lsWorker.stdout?.on('data', (chunk) => {
+// 		console.log('[ls stdout]', decodeChunk(chunk))
+// 	})
+// 	lsWorker.stderr?.on('data', (chunk) => {
+// 		console.error('[ls stderr]', decodeChunk(chunk))
+// 	})
+// 	lsWorker.onExit((code) => {
+// 		console.log('ls exited with code', code)
+// 	})
+// }
